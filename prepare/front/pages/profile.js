0a83af6cc679aa81e66254e946fbf9acda5c077f
ehbong <hangillw86@gmail.com>
2020-10-10 20:01:46 +0900

    init project

---
+++ prepare/front/pages/profile.js
@@ -0,0 +1,12 @@
+import AppLayout from "../conponents/AppLayout";
+import React from "react";
+
+const Profile = () => {
+    return (
+        <AppLayout>
+            <div>Hello, Profile!</div>
+        </AppLayout>
+    )
+}
+
+export default Profile;
---
+++ prepare/front/pages/signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AppLayout from "../conponents/AppLayout";
+
+const Signup = () => {
+    return (
+        <AppLayout><div>Hello, Signup!</div></AppLayout>
+    )
+}
+
+export default Signup;
