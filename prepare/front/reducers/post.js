ff6cb55f603c66ddce37d3fa58d8483468a0a171
ehbong <hangillw86@gmail.com>
2020-10-30 22:51:22 +0900

    set variable

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -37,10 +37,25 @@ export const initialState = {
   imagePaths: [],
   postAdded: false,
 };
+export const LOAD_POSTS_REQUEST = 'LOAD_POSTS_REQUEST';
+export const LOAD_POSTS_SUCCESS = 'LOAD_POSTS_SUCCESS';
+export const LOAD_POSTS_FAILURE = 'LOAD_POSTS_FAILURE';
 
-const ADD_POST = 'ADD_POST';
-export const addPost = {
-  type: ADD_POST,
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
+
+export const REMOVE_POST_REQUEST = 'REMOVE_POST_REQUEST';
+export const REMOVE_POST_SUCCESS = 'REMOVE_POST_SUCCESS';
+export const REMOVE_POST_FAILURE = 'REMOVE_POST_FAILURE';
+
+export const ADD_COMMENT_REQUEST = 'ADD_COMMENT_REQUEST';
+export const ADD_COMMENT_SUCCESS = 'ADD_COMMENT_SUCCESS';
+export const ADD_COMMENT_FAILURE = 'ADD_COMMENT_FAILURE';
+
+export const addPost = (data) => {
+  type: ADD_POST_REQUEST,
+  data,
 };
 
 const dummyPost = {
@@ -56,7 +71,19 @@ const dummyPost = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case ADD_POST:
+    case ADD_POST_REQUEST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    case ADD_POST_SUCCESS:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    case ADD_POST_FAILURE:
       return {
         ...state,
         mainPosts: [dummyPost, ...state.mainPosts],
--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -7,9 +7,36 @@ export const initialState = {
   loginData: {},
 };
 
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+export const CHANGE_NICKNAME_REQUEST = 'CHANGE_NICKNAME_REQUEST';
+export const CHANGE_NICKNAME_SUCCESS = 'CHANGE_NICKNAME_SUCCESS';
+export const CHANGE_NICKNAME_FAILURE = 'CHANGE_NICKNAME_FAILURE';
+
+export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+
+export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+
+export const ADD_POST_TO_ME = 'ADD_POST_TO_ME';
+export const REMOVE_POST_OF_ME = 'REMOVE_POST_OF_ME';
+
 export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN_REQUEST',
+    type: LOG_IN_REQUEST,
     data,
   };
 };
@@ -21,22 +48,22 @@ export const logoutRequestAction = () => {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'LOG_IN_REQUEST':
+    case LOG_IN_REQUEST:
       return { ...state, isLoggingIn: true, me: action.data };
-    case 'LOG_IN_SUCCESS':
+    case LOG_IN_SUCCESS:
       return {
         ...state,
         isLogin: true,
         isLoggingIn: false,
         me: { ...action.data, nickname: 'messi' },
       };
-    case 'LOG_IN_FAILURE':
+    case LOG_IN_FAILURE:
       return { ...state, isLoggingIn: false, me: action.data };
-    case 'LOG_OUT_SUCCESS':
+    case LOG_OUT_SUCCESS:
       return { ...state, isLoggingOut: false, isLogin: false, me: null };
-    case 'LOG_OUT_REQUEST':
+    case LOG_OUT_REQUEST:
       return { ...state, isLoggingOut: true, me: null };
-    case 'LOG_OUT_FAILURE':
+    case LOG_OUT_FAILURE:
       return { ...state, isLoggingOut: false, isLogin: false, me: null };
     default:
       return state;
