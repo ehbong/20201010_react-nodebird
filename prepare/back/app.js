a6fa03c2f92d6c1d46401c68ac206bfc427ba0d7
ehbong <hangillw86@gmail.com>
2020-11-04 21:04:44 +0900

    express basic server start

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,15 +1,41 @@
 /** @format */
 
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write("<h1>hello node</h1>");
-  res.write("<h2>hello node</h2>");
-  res.write("<h3>hello node</h3>");
-  res.write("<h4>hello node</h4>");
-  res.end("hello node");
-});
+const express = require("express");
+// express 서버 구동
+const app = express();
 const port = 3065;
-server.listen(port, () => {
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+app.get("/api", (req, res) => {
+  res.json([
+    { id: 1, content: "hello" },
+    { id: 2, content: "hello2" },
+    { id: 3, content: "hello3" },
+  ]);
+});
+app.get("/api/post", (req, res) => {
+  res.json([
+    { id: 1, content: "hello" },
+    { id: 2, content: "hello2" },
+    { id: 3, content: "hello3" },
+  ]);
+});
+app.post("/api/post", (req, res) => {
+  res.json([{ id: 1, content: "hello" }]);
+});
+app.delete("/api/post", (req, res) => {
+  res.json([{ id: 1 }]);
+});
+
+app.listen(port, () => {
   console.log("서버 실행 중", port);
 });
+
+// app.get 가져오기
+// app.post 생성
+// app.put 전체수정
+// app.delete 삭제
+// app.patch 부분수정
+// app.options 찔러보기
+// app.head 헤더만 가져오기
